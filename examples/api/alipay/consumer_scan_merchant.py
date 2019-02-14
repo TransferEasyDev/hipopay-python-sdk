799327025c953480e7620d2dd9b9d4879fac6265
kongfm <kongp3@outlook>
2019-02-14 15:06:14 +0800

    跟随文档修改参数

--- examples/api/alipay/consumer_scan_merchant.py
+++ examples/api/alipay/consumer_scan_merchant.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
         'out_trade_id': 'your_trade_id',        # 商户交易流水号  Y
         'amount': '100',                        # 支付单金额，单位为元，精度最多小数点后两位(如果是JPY和KRW，单位为分) Y
         'currency': 'HKD',                      # 结算币种 Y
-        'auth_code': '',                        # 二维码内容 Y
         'product_info': 'test product',         # 商品信息 Y
         'client_ip': '0.0.0.0',                 # 客户端设备IP地址 Y
         'notify_url': 'your_notify_url',        # 异步通知地址  N
