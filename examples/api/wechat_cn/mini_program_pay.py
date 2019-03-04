5cb026d29aee6d4ccdc122cc5fda14b38fb2d09b
kongfm <kongp3@outlook>
2019-03-04 15:11:07 +0800

    小程序添加merchant_no字段

--- examples/api/wechat_cn/mini_program_pay.py
+++ examples/api/wechat_cn/mini_program_pay.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from config import MERCHANT_MINI_APPID
+from config import MERCHANT_MINI_APPID, MERCHANT_NO
 from kernel.api.wechat_cn import WechatCn
 
 if __name__ == '__main__':
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     ''' 第二步 获取openid '''
     ''' 第三步 下单,调用下列接口,传入参数 '''
     params = {
-        'merchant_no': 'WC15241227ed28b',          # 商户交易流水号 Y
+        'merchant_no': MERCHANT_NO,          # 商户交易流水号 Y
         'out_trade_id': 'your_trande_id',          # 商户交易流水号 Y
         'amount': '1',                             # 支付单金额，单位为元，精度最多小数点后两位(如果是JPY和KRW，单位为分) Y
         'currency': 'HKD',                         # 支付单结算币种
