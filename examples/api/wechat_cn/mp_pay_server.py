a9dc82bd6e8034473849b8b5e70bd99ffeb55403
kongfm <kongp3@outlook>
2019-03-26 16:08:23 +0800

    添加公众号支付付款链接

---
+++ examples/api/wechat_cn/mp_pay_server.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request
+from flask import render_template
+
+app = Flask(__name__)
+app_host = '0.0.0.0'
+app_port = 8086
+
+
+'''为公众号支付提供h5页面'''
+
+
+@app.route('/mp_pay', methods=['GET', 'POST'])
+def mpay(**kwargs):
+    openid = request.values.get('openid')
+    merchant_no = request.values.get('merchant_no')
+    print openid
+    print merchant_no
+    d = {'meta': {
+        'success': True, 'status_code': 200,
+        'message': "Requset Successes"
+    }}
+    # json_response = jsonify(d)
+    # return json_response
+    return render_template("mp_pay_index.html", openid=openid, merchant_no=merchant_no)
+
+
+if __name__ == '__main__':
+    app.run(host=app_host, port=app_port)
